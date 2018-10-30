0846b15351eef083d1127814f4f8f2793442ef02
Stas Sribnyi <stas.sribnyi@gmail.com>
2018-10-30 20:21:32 +0200

    Add basic structure for tic tac toe

---
+++ tic-tac-toe/scripts/main.js
@@ -0,0 +1,27 @@
+import { Container, FPSCounter } from '../shared.js';
+
+const canvas = document.querySelector('canvas');
+const context = canvas.getContext('2d');
+const container = new Container();
+
+let fpsCounter = null;
+
+function initialize() {
+  container.width = canvas.width = window.innerWidth;
+  container.height = canvas.height = window.innerHeight;
+
+  fpsCounter = new FPSCounter(context);
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  context.clearRect(0, 0, container.width, container.height);
+  context.font = '12px PressStart2P';
+
+  context.fillStyle = 'rgba(40,44,62, 0.25)';
+  fpsCounter.update();
+}
+
+initialize();
+animate();
