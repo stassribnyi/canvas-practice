6243ddee2d789c4a018f441825c845f0e74f4772
Stas Sribnyi <stas.sribnyi@gmail.com>
2018-12-03 15:53:07 +0200

    Code cleanup

--- snake/scripts/main.js
+++ snake/scripts/main.js
@@ -6,12 +6,43 @@ import {
 } from '../../shared/index.js';
 import { SnakeGame } from './game/index.js';
 
-document.addEventListener('touchstart', handleTouchStart, { passive: false });
-document.addEventListener('touchmove', handleTouchMove, { passive: false });
+let loaded = false;
 
+window.addEventListener('touchstart', handleTouchStart, { passive: false });
+window.addEventListener('touchmove', handleTouchMove, { passive: false });
+window.addEventListener('keydown', handleKeyDown);
+window.addEventListener('resize', expand);
+window.addEventListener('load', () => {
+  loaded = true;
+  init();
+});
+
+let game = null;
+let canvas = null;
+let context = null;
+let fpsCounter = null;
 let initialTouch = null;
+let toggleFPSCounter = false;
+
+let isResizeConfirmOpened = false;
+
+function handleKeyDown({ keyCode }) {
+  if (!loaded) {
+    return;
+  }
+
+  if (keyCode === 70) {
+    toggleFPSCounter = !toggleFPSCounter;
+  }
+
+  game.handleMove(getKeyDirection(keyCode));
+}
 
 function handleTouchStart(event) {
+  if (!loaded) {
+    return;
+  }
+
   const { clientX, clientY } = event.touches[0];
 
   initialTouch = {
@@ -21,6 +52,10 @@ function handleTouchStart(event) {
 }
 
 function handleTouchMove(event) {
+  if (!loaded) {
+    return;
+  }
+
   const direction = getSwipeDirection(initialTouch, event.touches[0]);
 
   if (!direction) {
@@ -35,35 +70,12 @@ function handleTouchMove(event) {
   event.stopImmediatePropagation();
 }
 
-let loaded = false;
-
-window.addEventListener('resize', () => expand());
-window.addEventListener('keydown', ({ keyCode }) => handleKey(keyCode));
-window.addEventListener('load', () => {
-  loaded = true;
-  init();
-});
-
-let game = null;
-let canvas = null;
-let context = null;
-let fpsCounter = null;
-let toggleFPSCounter = false;
-
-let isResizeConfirmOpened = false;
-
-function handleKey(keyCode) {
-  if (keyCode === 70) {
-    toggleFPSCounter = !toggleFPSCounter;
-  }
-
-  game.handleMove(getKeyDirection(keyCode));
-}
-
 function expand() {
   let reset = false;
+
   if (!isResizeConfirmOpened) {
     isResizeConfirmOpened = true;
+
     reset = confirm(
       'You have just changed size of the screen, do you wanna resize game field? If yes, current game will be lost!'
     );
